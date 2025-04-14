4670b5fdf5fa69d3734c42968a3c315e66e72c13
CalleEklund <eklund.calle@gmail.com>
2025-04-14 18:35:02 +0200

    fix: persist subtitles choice

--- android/src/main/kotlin/media/bcc/bccm_player/players/exoplayer/ExoPlayerController.kt
+++ android/src/main/kotlin/media/bcc/bccm_player/players/exoplayer/ExoPlayerController.kt
@@ -2,6 +2,7 @@ package media.bcc.bccm_player.players.exoplayer
 
 import android.content.Context
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.WindowManager
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
@@ -9,17 +10,22 @@ import androidx.media3.common.C.VIDEO_SCALING_MODE_SCALE_TO_FIT
 import androidx.media3.common.ForwardingPlayer
 import androidx.media3.common.MediaItem
 import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
 import androidx.media3.common.TrackGroup
 import androidx.media3.common.Tracks
+import androidx.media3.common.text.Cue
+import androidx.media3.common.text.CueGroup
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.DefaultHttpDataSource
 import androidx.media3.datasource.cache.CacheDataSource
 import androidx.media3.exoplayer.DefaultLoadControl
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.LoadControl
+import androidx.media3.exoplayer.hls.HlsMediaSource
 import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
 import androidx.media3.ui.PlayerView
+import androidx.media3.ui.SubtitleView
 import com.npaw.youbora.lib6.media3.Media3Adapter
 import com.npaw.youbora.lib6.plugin.Options
 import com.npaw.youbora.lib6.plugin.Plugin
@@ -37,37 +43,66 @@ import media.bcc.bccm_player.pigeon.PlaybackPlatformApi.NpawConfig
 import media.bcc.bccm_player.players.PlayerController
 import media.bcc.bccm_player.players.chromecast.CastMediaItemConverter.Companion.PLAYER_DATA_IS_LIVE
 import media.bcc.bccm_player.players.chromecast.CastMediaItemConverter.Companion.PLAYER_DATA_IS_OFFLINE
-import media.bcc.bccm_player.utils.LanguageUtils
 import java.util.UUID
+import media.bcc.bccm_player.utils.SystemGestureExcludedLinearLayout
+import media.bcc.bccm_player.R
 
 @UnstableApi
 class ExoPlayerController(
     private val context: Context,
     bufferMode: BufferMode,
     private val disableNpaw: Boolean = false
-) :
-    PlayerController() {
+) : PlayerController() {
+
     override val id: String = UUID.randomUUID().toString()
     private val trackSelector: DefaultTrackSelector = DefaultTrackSelector(context)
+    private val cacheDataSourceFactory = CacheDataSource.Factory()
+        .setCache(Downloader.getCache(context))
+        .setUpstreamDataSourceFactory(DefaultHttpDataSource.Factory())
+        .setCacheWriteDataSinkFactory(null)
+    private val hlsFactory = HlsMediaSource.Factory(cacheDataSourceFactory)
+        .setAllowChunklessPreparation(false)
+    var currentSelectedLanguage: String = ""
     private val exoPlayer: ExoPlayer = ExoPlayer.Builder(context)
         .setTrackSelector(trackSelector)
         .setAudioAttributes(AudioAttributes.DEFAULT, true)
         .setVideoScalingMode(VIDEO_SCALING_MODE_SCALE_TO_FIT)
-        .setMediaSourceFactory(
-            DefaultMediaSourceFactory(context).setDataSourceFactory(
-                CacheDataSource.Factory()
-                    .setCache(Downloader.getCache(context))
-                    .setUpstreamDataSourceFactory(DefaultHttpDataSource.Factory())
-                    .setCacheWriteDataSinkFactory(null)
-            )
-        )
+        .setMediaSourceFactory(hlsFactory)
         .setLoadControl(getLoadControlForBufferMode(bufferMode))
         .build()
+
+    init {
+        exoPlayer.addListener(object : Player.Listener {
+            override fun onCues(cueGroup: CueGroup) {
+                val cues = cueGroup.cues
+                val subTitleCues = cues.mapNotNull { cue ->
+                    cue.text?.let {
+                        PlaybackPlatformApi.SubtitleCue.Builder()
+                            .setStartTimeMs(player.currentPosition.toLong())
+                            .setEndTimeMs(0)
+                            .setText(it.toString())
+                            .build()
+                    }
+                }
+                val subtitleEvent = PlaybackPlatformApi.SubtitleEvent.Builder()
+                    .setPlayerId(id)
+                    .setLanguage(currentSelectedLanguage)
+                    .setCues(subTitleCues)
+                    .build()
+                sendCues(subtitleEvent)
+            }
+
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                if (playbackState == Player.STATE_READY) {
+                    Log.d("MySubtitles", "Player is ready")
+                }
+            }
+        })
+    }
 
     override val player: ForwardingPlayer
     override var currentPlayerViewController: BccmPlayerViewController? = null
     private var textLanguagesThatShouldBeSelected: Array<String>? = null
-
     private var _currentPlayerView: PlayerView? = null
     private var currentPlayerView: PlayerView?
         get() = _currentPlayerView
@@ -98,15 +133,15 @@ class ExoPlayerController(
     init {
         player = BccmForwardingPlayer(this)
         player.addListener(this)
-
+        trackSelector.setParameters(
+            trackSelector.buildUponParameters().build()
+        )
         handleUpdatedAppConfig(BccmPlayerPluginSingleton.appConfigState.value)
         BccmPlayerPluginSingleton.npawConfigState.value?.let {
             handleUpdatedNpawConfig(it)
         }
         mainScope.launch {
-            BccmPlayerPluginSingleton.npawConfigState.collect {
-                handleUpdatedNpawConfig(it)
-            }
+            BccmPlayerPluginSingleton.npawConfigState.collect { handleUpdatedNpawConfig(it) }
         }
         mainScope.launch {
             BccmPlayerPluginSingleton.appConfigState.collect { handleUpdatedAppConfig(it) }
@@ -115,12 +150,8 @@ class ExoPlayerController(
 
     override fun onPlayerError(error: PlaybackException) {
         if (error.errorCode == PlaybackException.ERROR_CODE_BEHIND_LIVE_WINDOW) {
-            // Re-initialize player at the current live window default position.
-            // https://exoplayer.dev/live-streaming.html#behindlivewindowexception-and-error_code_behind_live_window
-            player.seekToDefaultPosition();
-            player.prepare();
-        } else {
-            // Handle other errors.
+            player.seekToDefaultPosition()
+            player.prepare()
         }
     }
 
@@ -132,17 +163,12 @@ class ExoPlayerController(
     }
 
     private fun handleUpdatedAppConfig(appConfigState: PlaybackPlatformApi.AppConfig?) {
-        Log.d(
-            "bccm",
-            "setting preferred audio and sub lang to: ${appConfigState?.audioLanguages}, ${appConfigState?.subtitleLanguages}"
-        )
+        Log.d("ExoPlayerController", "handleUpdatedAppConfig called with: $appConfigState")
         var audioLanguages = getExpectedAudioLanguages(appConfigState)
         val isPrimary = plugin?.getPlaybackService()?.getPrimaryController()?.id == id
-
         if (
-            player.trackSelectionParameters.preferredAudioLanguages != audioLanguages
-            && isPrimary
-            && appConfigState != null
+            player.trackSelectionParameters.preferredAudioLanguages != audioLanguages &&
+            isPrimary && appConfigState != null
         ) {
             audioLanguages = appConfigState.audioLanguages
             manuallySelectedAudioLanguage = null
@@ -150,13 +176,10 @@ class ExoPlayerController(
                 .clearOverridesOfType(C.TRACK_TYPE_AUDIO)
                 .build()
         }
-
         player.trackSelectionParameters = trackSelector.parameters.buildUpon()
             .setPreferredAudioLanguages(*(audioLanguages.toTypedArray()))
             .build()
-
-        textLanguagesThatShouldBeSelected =
-            appConfigState?.subtitleLanguages?.toTypedArray()
+        textLanguagesThatShouldBeSelected = appConfigState?.subtitleLanguages?.toTypedArray()
         updateYouboraOptions()
     }
 
@@ -177,16 +200,16 @@ class ExoPlayerController(
             Log.d("bccm", "ExoPlayerController: Youbora is disabled")
             return
         }
-        Log.d("bccm", "ExoPlayerController: Initializing youbora")
-        val options = Options()
-        options.isAutoDetectBackground = false
-        options.userObfuscateIp = true
-        options.isParseManifest = true
-        options.isEnabled = true
-        options.accountCode = config.accountCode
-        options.appReleaseVersion = config.appReleaseVersion
-        options.appName = config.appName
-        options.deviceIsAnonymous = config.deviceIsAnonymous ?: false
+        val options = Options().apply {
+            isAutoDetectBackground = false
+            userObfuscateIp = true
+            isParseManifest = true
+            isEnabled = true
+            accountCode = config.accountCode
+            appReleaseVersion = config.appReleaseVersion
+            appName = config.appName
+            deviceIsAnonymous = config.deviceIsAnonymous ?: false
+        }
         youboraPlugin = Plugin(options, context).also {
             it.adapter = Media3Adapter(exoPlayer)
         }
@@ -194,48 +217,36 @@ class ExoPlayerController(
     }
 
     fun updateYouboraOptions() {
-        val youboraPlugin = youboraPlugin ?: return
-        Log.d(
-            "bccm",
-            "ExoPlayerController: Updating youbora options: ${player.mediaMetadata.title}"
-        )
-
-        // Metadata options
+        val yPlugin = youboraPlugin ?: return
         val mediaMetadata = player.mediaMetadata
         val extras = mediaMetadata.extras?.let { extractExtrasFromAndroid(it) }
-        youboraPlugin.options.contentIsLive =
+        yPlugin.options.contentIsLive =
             extras?.get("npaw.content.isLive")?.toBooleanStrictOrNull()
-                ?: player.mediaMetadata.extras?.getString(PLAYER_DATA_IS_LIVE)
-                    ?.toBooleanStrictOrNull()
-                        ?: player.isCurrentMediaItemLive
-        youboraPlugin.options.contentId = extras?.get("npaw.content.id")
+                ?: player.mediaMetadata.extras?.getString(PLAYER_DATA_IS_LIVE)?.toBooleanStrictOrNull()
+                ?: player.isCurrentMediaItemLive
+        yPlugin.options.contentId = extras?.get("npaw.content.id")
             ?: mediaMetadata.extras?.getString("id")
-        youboraPlugin.options.contentTitle = extras?.get("npaw.content.title")
+        yPlugin.options.contentTitle = extras?.get("npaw.content.title")
             ?: mediaMetadata.title?.toString() ?: mediaMetadata.displayTitle?.toString()
-        youboraPlugin.options.contentTvShow = extras?.get("npaw.content.tvShow")
-        youboraPlugin.options.contentSeason = extras?.get("npaw.content.season")
-        youboraPlugin.options.contentEpisodeTitle = extras?.get("npaw.content.episodeTitle")
-        youboraPlugin.options.isOffline =
+        yPlugin.options.contentTvShow = extras?.get("npaw.content.tvShow")
+        yPlugin.options.contentSeason = extras?.get("npaw.content.season")
+        yPlugin.options.contentEpisodeTitle = extras?.get("npaw.content.episodeTitle")
+        yPlugin.options.isOffline =
             extras?.get("npaw.isOffline")?.toBooleanStrictOrNull()
-                ?: player.mediaMetadata.extras?.getString(PLAYER_DATA_IS_OFFLINE)
-                    ?.toBooleanStrictOrNull() ?: false
-        youboraPlugin.options.contentType = extras?.get("npaw.content.type")
-        youboraPlugin.options.contentTransactionCode = extras?.get("npaw.content.transactionCode")
-
-        // App config based options
+                ?: player.mediaMetadata.extras?.getString(PLAYER_DATA_IS_OFFLINE)?.toBooleanStrictOrNull()
+                ?: false
+        yPlugin.options.contentType = extras?.get("npaw.content.type")
         val appConfig = BccmPlayerPluginSingleton.appConfigState.value
-        youboraPlugin.options.username = appConfig?.analyticsId
-        youboraPlugin.options.contentCustomDimension1 = extras?.get("npaw.content.customDimension1")
-            ?: if (appConfig?.sessionId != null) appConfig.sessionId.toString() else null
-        youboraPlugin.options.contentCustomDimension2 = extras?.get("npaw.content.customDimension2")
-
-        for (t in player.currentTracks.groups) {
-            if (!t.isSelected) continue
-
-            if (t.type == C.TRACK_TYPE_TEXT) {
-                youboraPlugin.options?.contentSubtitles = LanguageUtils.toThreeLetterLanguageCode(t.mediaTrackGroup.getFormat(0).language)
-            } else if (t.type == C.TRACK_TYPE_AUDIO) {
-                youboraPlugin.options?.contentLanguage = LanguageUtils.toThreeLetterLanguageCode(t.mediaTrackGroup.getFormat(0).language)
+        yPlugin.options.username = appConfig?.analyticsId
+        yPlugin.options.contentCustomDimension1 = extras?.get("npaw.content.customDimension1")
+            ?: appConfig?.sessionId?.toString()
+        yPlugin.options.contentCustomDimension2 = extras?.get("npaw.content.customDimension2")
+        for (group in player.currentTracks.groups) {
+            if (!group.isSelected) continue
+            if (group.type == C.TRACK_TYPE_TEXT) {
+                yPlugin.options?.contentSubtitles = group.mediaTrackGroup.getFormat(0).language
+            } else if (group.type == C.TRACK_TYPE_AUDIO) {
+                yPlugin.options?.contentLanguage = group.mediaTrackGroup.getFormat(0).language
             }
         }
     }
@@ -244,23 +255,10 @@ class ExoPlayerController(
         return exoPlayer
     }
 
-    /**
-     * This function forces the lowest video bitrate to be used.
-     *
-     * This was originally a "setRendererDisabled" function.
-     * Expected it to stop loading video segments if you disable the renderer,
-     * but it doesn't. This is still an open issue: https://github.com/google/ExoPlayer/issues/9282
-     */
     private fun setForceLowestVideoBitrate(force: Boolean) {
         val parametersBuilder = trackSelector.buildUponParameters()
         parametersBuilder.setForceLowestBitrate(force)
-
         trackSelector.setParameters(parametersBuilder)
-
-        Log.d(
-            "bccm",
-            if (force) "Forcing lowest bitrate" else "No longer forcing lowest bitrate"
-        )
     }
 
     private fun getLowestBitrateTrackIndex(trackGroup: TrackGroup): Int {
@@ -278,15 +276,12 @@ class ExoPlayerController(
 
     fun takeOwnership(playerView: PlayerView, viewController: BccmPlayerViewController) {
         if (surface != null) {
-            Log.w(
-                "bccm",
-                "takeOwnership called but the player is rendering to a custom surface. Remove the texture first with removeTexture(). Aborting."
-            )
+            Log.w("bccm", "takeOwnership called but the player is rendering to a custom surface. Remove it first.")
             return
         }
         if (currentPlayerView != null && currentPlayerView != playerView) {
             PlayerView.switchTargetView(player, currentPlayerView, playerView)
-            currentPlayerViewController?.onOwnershipLost();
+            currentPlayerViewController?.onOwnershipLost()
         } else {
             playerView.player = player
         }
@@ -312,9 +307,17 @@ class ExoPlayerController(
     }
 
     override fun onTracksChanged(tracks: Tracks) {
-        // We use this callback to set the default language for the subtitles.
-        // we only set the default text language once when there is no track selected already and
-        // the language is available for the current track.
+        super.onTracksChanged(tracks)
+        for (group in tracks.groups) {
+            if (group.type == C.TRACK_TYPE_TEXT) {
+                for (i in 0 until group.length) {
+                    val format = group.mediaTrackGroup.getFormat(i)
+                    if (group.isSelected) {
+                        currentSelectedLanguage = format.language.toString()
+                    }
+                }
+            }
+        }
         val textLanguages = textLanguagesThatShouldBeSelected
         if (!textLanguages.isNullOrEmpty() && tracks.groups.any { it.type == C.TRACK_TYPE_TEXT }) {
             if (setSelectedTrackByLanguages(C.TRACK_TYPE_TEXT, textLanguages, tracks)) {
@@ -322,7 +325,7 @@ class ExoPlayerController(
             }
         }
         if (textLanguages?.isEmpty() == true) {
-            setTrackTypeDisabled(C.TRACK_TYPE_TEXT, true);
+            setTrackTypeDisabled(C.TRACK_TYPE_TEXT, true)
             textLanguagesThatShouldBeSelected = null
         }
         updateYouboraOptions()
@@ -338,20 +341,12 @@ class ExoPlayerController(
 
     private fun getLoadControlForBufferMode(bufferMode: BufferMode): LoadControl {
         return when (bufferMode) {
-            BufferMode.STANDARD -> DefaultLoadControl.Builder().setBufferDurationsMs(
-                5000,
-                50000,
-                500,
-                2000
-            ).setPrioritizeTimeOverSizeThresholds(true).build()
-
+            BufferMode.STANDARD -> DefaultLoadControl.Builder()
+                .setBufferDurationsMs(5000, 50000, 500, 2000)
+                .setPrioritizeTimeOverSizeThresholds(true)
+                .build()
             BufferMode.FAST_START_SHORT_FORM -> DefaultLoadControl.Builder()
-                .setBufferDurationsMs(
-                    5000,
-                    40000,
-                    500,
-                    2000
-                )
+                .setBufferDurationsMs(5000, 40000, 500, 2000)
                 .setPrioritizeTimeOverSizeThresholds(true)
                 .build()
         }
